704d1792b3072a2b20be8915dfb14d256ee2ba5a
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-08 21:53:02 +0200

    Add files via upload

---
+++ УппП1-Р/Дом Работа/Задача 1/Задача 1/Задача 1.cpp
@@ -0,0 +1,21 @@
+﻿/*
+Задача, в която се въвежда число.Ако числото е
+положително, програмата извежда 1. В противен случай
+извежда 0;
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int output;
+    cout << "Въведете число: ", cin >> n;
+    if (n > 0) {
+        output = 1;
+    }
+    else {
+        output = 0;
+    }
+    cout << output;
+}
